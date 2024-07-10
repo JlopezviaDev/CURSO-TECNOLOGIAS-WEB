63d2ab7dd5dec70b4b3e02f776a884e34a701eeb
JlopezviaDev <jlopezvia@gmail.com>
2024-07-10 19:00:01 +0200

    09/07/24

---
+++ MODULO_1/UNIDAD_2_JS/sorteo/main.js
@@ -0,0 +1,34 @@
+const inputName = document.getElementById('inputName');
+const addBtn = document.getElementById('addBtn');
+const nameList = document.getElementById('nameList');
+const randomBtn = document.getElementById('randomBtn');
+
+let names = [];
+
+function addName() {
+  const name = inputName.value.trim();
+  console.log(name)
+  if (name !== '') {
+    names.push(name);
+    inputName.value = '';
+    const listItem = document.createElement('li');
+    listItem.textContent = name;
+    listItem.id = name;
+    nameList.appendChild(listItem);
+  }
+}
+function selectRandomName() {
+  if (names.length > 0) {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const selectedName = names[randomIndex];
+    const selectedListItem = document.getElementById(selectedName);
+    selectedListItem.style.backgroundColor = "yellow"
+    selectedListItem.textContent += ` - Ganador: ${selectedName}`;
+  } else {
+    alert('¡No hay nombres en la lista!');
+  }
+}
+
+addBtn.addEventListener('click', addName);
+randomBtn.addEventListener('click', selectRandomName);
+console.log(Math.floor(Math.random()*3));
